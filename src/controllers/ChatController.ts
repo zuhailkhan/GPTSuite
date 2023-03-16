21275c8573d1a4ade339b13097a1fbca02dce51e
zuhailkhan <zuhail.khan@appstone.in>
2023-03-17 05:09:34 +0530

    Added Chat Streaming and model listing routes

--- src/controllers/ChatController.ts
+++ src/controllers/ChatController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { Configuration, OpenAIApi } from 'openai'
+// @ts-ignore
+import { EventSource } from 'launchdarkly-eventsource'
  
 const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
@@ -32,41 +34,80 @@ const basicResponse = async (req: Request, res: Response ) => {
     }
 }
 
-// const streamResponse = async (req: Request, res: Response ) => {
-//     const url = `https://api.openai.com/v1/completions`
-//     const messages = req.body.messages;
+const streamResponse = async (req: Request, res: Response ) => {
+    const url = `https://api.openai.com/v1/chat/completions`
+    const messages = [
+        {
+            "role": "user",
+            "content": "Count from 1 to 2"
+        }
+    ]
+    const evs = new EventSource(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+        },
+        body: JSON.stringify({
+            messages,
+            model: 'gpt-3.5-turbo',
+            stream: true
+        })
+    })
 
+    evs.addEventListener('message', (event: any) => {
+        if(event.data === `[DONE]`) evs.close()
+        res.write(`data: ${event.data}\n\n`)
+    })
+
+    evs.addEventListener('error', (event: any) => {
+        evs.close()
+        res.end()
+    })
+
+    evs.addEventListener('abort', (event: any) => {
+        evs.close()
+        res.end();
+    });
+
+    evs.addEventListener('close', () => {
+        res.end();
+    })
+
+    req.on('close', () => {
+        evs.close();
+    });
+
+    res.on('error', () => {
+        evs.close();
+    });
+}
+
+
+const listModels = async(req: Request, res: Response) => {
+    try {
+        const response = await openai.listModels();
+        return res.status(200).json(response.data)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+}
+
+// const retrieveModel = async(req: Request, res: Response) => {
 //     try {
-//         const response = await openai.createChatCompletion({
-//             model: 'gpt-3.5-turbo',
-//             messages,
-//             stream: true
-//         })
-//         // @ts-ignore
-//         const evs = new EventSource(response.data)
-//         evs.addEventListener('message', (event: any) => {
-//             res.setHeader('Content-Type', 'text/event-stream');
-//             res.setHeader('Cache-Control', 'no-cache');
-//             res.setHeader('Connection', 'keep-alive');
-//             res.write(event)
-//             // @ts-ignore
-//             res.flush()
-//         })
+//         const response = await openai.retrieveModel(req.body.model)
 
-//         evs.addEventListener('error', (event: any) => {
-//             console.log(event)
-//         })
-        
-//     }
-//     catch (error) {
-//         console.log(error)
-//         return res.status(500).json(error)
 //     }
 // }
 
 
 
+
+
 export default {
     basicResponse,
-    healthCheck
+    streamResponse,
+    healthCheck,
+    listModels,
 }
--- src/routes/ChatRoute.ts
+++ src/routes/ChatRoute.ts
@@ -3,10 +3,12 @@ import ChatController  from "../controllers/ChatController";
 import authenticator from "../middlewares/authenticator";
 const ChatRoute = express.Router();
 
-ChatRoute.get('/health', authenticator, ChatController.healthCheck)
+ChatRoute.get('/health', authenticator, ChatController.healthCheck);
 
-ChatRoute.post('/talkBasic', ChatController.basicResponse);
+ChatRoute.post('/talkBasic', authenticator, ChatController.basicResponse);
 
-// ChatRoute.post('/talkStream', ChatController.streamResponse);
+ChatRoute.post('/talkStream', authenticator, ChatController.streamResponse);
+
+ChatRoute.get('/listModels', authenticator, ChatController.listModels);
 
 export default ChatRoute;
